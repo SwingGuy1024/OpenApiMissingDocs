7dd75dc435493bfd1897b7d463fe17ac4737ab85
SwingGuy1024 <SwingGuy1024@yahoo.com>
2021-01-12 14:17:30 -0800

    Initial commit

---
+++ src/main/java/com/demo/sim/controller/LoginApiController.java
@@ -0,0 +1,24 @@
+package com.demo.sim.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-01-12T11:31:50.526522-08:00[America/Los_Angeles]")
+@Controller
+@RequestMapping("${openapi.customerOrders.base-path:}")
+public class LoginApiController implements LoginApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public LoginApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
